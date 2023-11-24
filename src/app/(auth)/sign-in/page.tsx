71b8c0fa434912e8ee12faa557fae3e282a2d070
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-11-24 20:04:22 +0530

    dynamic navbar

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -16,9 +16,22 @@ import {
 import { trpc } from '@/trpc/client';
 import { toast } from 'sonner';
 import { ZodError } from 'zod';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const Page = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const isSeller = searchParams.get('as') === 'seller';
+  const origin = searchParams.get('origin');
+
+  const continueAsSeller = () => {
+    router.push('?as=seller');
+  };
+
+  const continueAsBuyer = () => {
+    router.replace('/sign-in', undefined);
+  };
+
   const {
     register,
     handleSubmit,
@@ -27,24 +40,28 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidators),
   });
 
-  const router = useRouter();
-
-  const { mutate, isLoading } = trpc.auth.createPayloaduser.useMutation({
+  const { mutate: signInUser, isLoading } = trpc.auth.signIn.useMutation({
     onError: (err) => {
-      if (err.data?.code === 'CONFLICT') {
-        toast.error('This email is already in use. Sign in instead.');
-        return;
+      if (err.data?.code === 'UNAUTHORIZED') {
+        toast.error('Invalid credentials.');
       }
-      if (err instanceof ZodError) {
-        toast.error(err.issues[0].message);
-        return;
-      }
-      toast.error('Something went wrong. Please try again.');
     },
 
-    onSuccess: ({ sentToEmail }) => {
-      toast.success(`Verification email sent to ${sentToEmail}.`);
-      router.push('/verify-email?to=' + sentToEmail);
+    onSuccess: () => {
+      toast.success(`Signed in successfully!`);
+
+      if (origin) {
+        router.push(`/${origin}`);
+        return;
+      }
+
+      if (isSeller) {
+        router.push('/sell');
+        return;
+      }
+
+      router.push('/');
+      router.refresh();
     },
   });
 
@@ -52,7 +69,7 @@ const Page = () => {
     email,
     password,
   }: TAuthCredentialsValidators) => {
-    mutate({ email, password });
+    signInUser({ email, password });
   };
 
   return (
@@ -61,16 +78,18 @@ const Page = () => {
         <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
           <div className='flex flex-col items-center space-y-2 text-center'>
             <Icons.logo className='h-20 w-20' />
-            <h1 className='text-2xl font-bold'>Create an account</h1>
+            <h1 className='text-2xl font-bold'>
+              Sign in to your {isSeller ? 'seller' : ''} account
+            </h1>
 
             <Link
-              href={'/sign-in'}
+              href={'/sign-up'}
               className={buttonVariants({
                 variant: 'link',
                 className: 'gap-1.5',
               })}
             >
-              Already have an account? Sign-in
+              Don&apos;t have an account?
               <ArrowRightIcon className='h-4 w-4' />
             </Link>
           </div>
@@ -110,9 +129,42 @@ const Page = () => {
                   )}
                 </div>
 
-                <Button>Sign Up</Button>
+                <Button>Sign In</Button>
               </div>
             </form>
+
+            <div className='relative'>
+              <div
+                aria-hidden='true'
+                className='absolute inset-0 flex items-center'
+              >
+                <span className='w-full border-t' />
+              </div>
+
+              <div className='relative flex justify-center text-xs uppercase'>
+                <span className='bg-background px-2 text-muted-foreground'>
+                  Or
+                </span>
+              </div>
+            </div>
+
+            {isSeller ? (
+              <Button
+                onClick={continueAsBuyer}
+                variant={'secondary'}
+                disabled={isLoading}
+              >
+                Continue as customer
+              </Button>
+            ) : (
+              <Button
+                onClick={continueAsSeller}
+                variant={'secondary'}
+                disabled={isLoading}
+              >
+                Continue as seller
+              </Button>
+            )}
           </div>
         </div>
       </div>
