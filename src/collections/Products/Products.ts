377bb218111cdf3b737427672b166f81bdfdbd03
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-11-25 09:00:39 +0530

    fetch products

---
+++ src/collections/Products/Products.ts
@@ -0,0 +1,119 @@
+import { PRODUCT_CATEGORIES } from '../../config';
+import { CollectionConfig } from 'payload/types';
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {},
+
+  fields: [
+    {
+      name: 'user',
+      type: 'relationship',
+      relationTo: 'users',
+      required: true,
+      hasMany: false,
+      admin: { condition: () => false },
+    },
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      label: 'Product details',
+      type: 'textarea',
+    },
+    {
+      name: 'price',
+      label: 'Price in Rupee',
+      type: 'number',
+      min: 0,
+      max: 5000,
+      required: true,
+    },
+    {
+      name: 'category',
+      label: 'Category',
+      type: 'select',
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    {
+      name: 'product_files',
+      label: 'Product Files',
+      type: 'relationship',
+      relationTo: 'product_files',
+      hasMany: false,
+      required: true,
+    },
+    {
+      name: 'approvedForSale',
+      label: 'Product Status',
+      type: 'select',
+      defaultValue: 'pending',
+      options: [
+        {
+          label: 'Pending Verification',
+          value: 'pending',
+        },
+        {
+          label: 'Approved',
+          value: 'approved',
+        },
+        {
+          label: 'Denied',
+          value: 'denied',
+        },
+      ],
+      access: {
+        create: ({ req }) => req.user.role === 'admin',
+        read: ({ req }) => req.user.role === 'admin',
+        update: ({ req }) => req.user.role === 'admin',
+      },
+    },
+    {
+      name: 'priceId',
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: 'text',
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: 'stipeId',
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: 'text',
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: 'images',
+      type: 'array',
+      label: 'Product Images',
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: 'Image',
+        plural: 'Images',
+      },
+      fields: [
+        { name: 'image', type: 'upload', relationTo: 'media', required: true },
+      ],
+    },
+  ],
+};
